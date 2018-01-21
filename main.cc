49d4a0c6abc9c6b10109b5cf82aa28854053d679
chankyu.choi <nagyojang@gmail.com>
2018-01-21 11:01:23 +0000

    delete legacy

--- main.cc
+++ main.cc
@@ -31,79 +31,8 @@ int main() {
                                                        metagraph,
                                                        s);
 
-    cout << TF_GetCode(s) << " != TF_OK(" << TF_OK << ")" << endl;
     cout << TF_Message(s) << endl;
-    //cout << "loaded the saved model" << endl;
-    return 1;
-
-    // Input Images
-    const int kBatchSize = 1;
-    const int kImageSize = 200;
-    const int kImageChannels = 3;
-    const long images_dims[] = {kBatchSize, kImageSize, kImageSize, kImageChannels};
-    const int images_num_dims = 4;
-    const size_t images_data_size = kBatchSize*kImageSize*kImageSize*kImageChannels*sizeof(float);
-    float* images_data = new float[images_data_size];
-    TF_Tensor* images_tensor = TF_NewTensor(TF_FLOAT, 
-                                            images_dims,
-                                            images_num_dims,
-                                            images_data,
-                                            images_data_size,
-                                            NULL, NULL);
-    cout << "made input images" << endl;
-    return -1;
     
-    // Input Labels
-    const int kSeqLength = 10;
-    const long labels_dims[] = {kBatchSize, kSeqLength};
-    const int labels_num_dims = 2;
-    const size_t labels_data_size = kBatchSize*kSeqLength*sizeof(long);
-    int64_t* labels_data = new int64_t[labels_data_size];
-    TF_Tensor* labels_tensor = TF_NewTensor(TF_INT64, 
-                                            labels_dims,
-                                            labels_num_dims,
-                                            labels_data,
-                                            labels_data_size,
-                                            NULL,
-                                            NULL);
-
-  
-    cout << "hello world" << endl; 
-    return -1;
-    // Output
-    struct TF_Output * output;
-    
-    // Session Run
-    vector<TF_Output> inputs;
-    vector<TF_Tensor*> input_values;
-    input_values.push_back(images_tensor); 
-    input_values.push_back(labels_tensor); 
-
-    vector<TF_Output> outputs;
-    TF_Operation* output_op = TF_GraphOperationByName(graph, "import/predict");
-    outputs.push_back({output_op, 0});
-    vector<TF_Tensor*> output_values(outputs.size(), NULL);
-
-
-    TF_Operation* target_opers = NULL;
-    int num_targets = 0;
-    TF_Buffer* handle = NULL;
-
-    TF_SessionRun(session, run_options,
-                  &inputs[0], &input_values[0], inputs.size(),
-                  &outputs[0], &output_values[0], outputs.size(),
-                  &target_opers, num_targets,
-                  handle, s);
-
-
-    void* output_data = TF_TensorData(output_values[0]);
-    assert(TF_GetCode(s) == TF_OK);
-
-    for (int i = 0; i < inputs.size(); ++i) TF_DeleteTensor(input_values[i]);
-    for (int i = 0; i < outputs.size(); ++i) TF_DeleteTensor(output_values[i]);
-
-    //TF_CloseSession(session, s); 
-    //TF_DeleteSession(session, s);
     TF_DeleteBuffer(metagraph);
     TF_DeleteGraph(graph);
     TF_DeleteStatus(s);
